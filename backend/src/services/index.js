00a5f4599cbdd473c04df1e11158eaec1b71932e
Morgan Benton <morgan.benton@gmail.com>
2018-12-05 09:48:15 -0500

    added all services

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,3 +1,9 @@
+const courses = require('./courses/courses.service.js');
+const students = require('./students/students.service.js');
+const subjects = require('./subjects/subjects.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
+  app.configure(courses);
+  app.configure(students);
+  app.configure(subjects);
 };
---
+++ backend/src/services/subjects/subjects.service.js
@@ -0,0 +1,24 @@
+// Initializes the `subjects` service on path `/subjects`
+const createService = require('feathers-knex');
+const createModel = require('../../models/subjects.model');
+const hooks = require('./subjects.hooks');
+
+module.exports = function (app) {
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'subjects',
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/subjects', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('subjects');
+
+  service.hooks(hooks);
+};
+  

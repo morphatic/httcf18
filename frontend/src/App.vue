3157746809d4941524540d44328def22211103ea
Morgan Benton <morgan.benton@gmail.com>
2018-12-07 10:04:00 -0500

    added navigation and routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,57 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
+    <v-navigation-drawer
+      app
+      fixed
+      clipped
+      persistent
+      :mini-variant="miniVariant"
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile 
+          v-for="(route, i) in routes.filter(r => r.meta && r.meta.icon)" 
+          :key="i" 
+          :to="route.path"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="route.meta.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ route.name }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-btn icon @click.stop="miniVariant = !miniVariant">
+            <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+          </v-btn>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app clipped-left class="primary white--text">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase" v-text="title"></v-toolbar-title>
     </v-toolbar>
 
     <v-content>
       <router-view/>
     </v-content>
+    <v-footer app fixed inset>
+      <span>&copy; 2018 Morgan C. Benton, All rights reserved.</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data () {
     return {
-      //
+      drawer: false,
+      routes: this.$router.options.routes,
+      miniVariant: false,
+      title: "HTTC"
     }
   }
 }
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Courses from "./views/courses/Courses.vue";
+import Course from "./views/courses/Course.vue";
+import NewCourse from "./views/courses/NewCourse.vue";
 
 Vue.use(Router);
 
@@ -11,16 +15,36 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      meta: {
+        icon: "home"
+      }
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About,
+      meta: {
+        icon: "info"
+      }
+    },
+    {
+      path: "/courses",
+      name: "courses",
+      component: Courses,
+      meta: {
+        icon: "school"
+      }
+    },
+    {
+      path: "/courses/:id",
+      name: "course",
+      component: Course
+    },
+    {
+      path: "/courses/new",
+      name: "new_course",
+      component: NewCourse
     }
   ]
 });
